cabc7d5facf0b39058d38018a0d6cfa578c35b6a
Gaurav Sharma <gau1711sha@gmail.com>
2025-05-27 00:13:20 +0530

    delete notification/notifications controller completed

--- backend/src/controllers/notification.controller.js
+++ backend/src/controllers/notification.controller.js
@@ -1,3 +1,4 @@
+
 import Notification from '../models/notification.model.js'
 
 export const getNotiofications = async(req,res)=>{
@@ -26,9 +27,42 @@ response.status(200).json(notification);
 
 export const deleteNotiofications = async()=>{
     try {
-        
+        const userId = req.user._id;
+
+        await Notification.deleteMany({to:userId});
+
+        res.status(200).json({message:"Notification deelted successfully"})
+
     } catch (error) {
         console.log("Error in delete Notifications controller",error.message);
+        res.status(500).json({error:"Internal server error"})
+        
+    }
+}
+
+
+export const deleteNotiofication = async()=>{
+    try {
+        const userId = req.user._id;
+        const notificationId = req.params.id;
+        const notification = await Notification.findById(notificationId)
+
+        if(!notification){
+            return res.status(404).json({error:"notification not found"})
+        }
+
+
+ if(notification.to.toString() !== userId.toString()){
+    return res.status(403).json({error:"you are not authorized to delete this notification"})
+        }
+
+await Notification.findByIdAndDelete(notificationId)
+
+res.status(200).json({message:"notification deleted successfully"});
+
+
+    } catch (error) {
+        console.log("Error in delete Notification controller",error.message);
         res.status(500).json({error:"Internal server error"})
         
     }
